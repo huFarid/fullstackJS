4e00cbb8bce94cd920172cdf2ea0d14fd71b2be9
Hossein Younesian Farid <h.younesian.farid@gmail.com>
2026-01-24 22:14:57 +0100

    New Component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,48 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const StatisticLine = ({text, value})=>
+  {
+    return(
+      <p>{text} {value}</p>     
+    )
+  }
+
 const Statistics = ({goodCount, neutralCount, badCount})=>
 {
-
+  const all  = goodCount + neutralCount + badCount
+  const average = all > 0 ? (goodCount - badCount) / all :0
+  const positive = all > 0 ? (goodCount / all) * 100 : 0
   return(
   <>
       <h2>Statistics</h2>
-
-      <p>good</p>
-      <p >{goodCount}</p>
-      <p>neutral</p>
-      <p>{neutralCount}</p>
-      <p>bad</p>
-      <p>{badCount}</p>
-      <p>all</p>
-      <p>{goodCount + neutralCount + badCount}</p>
-      <p>average</p>
-      <p>{goodCount + neutralCount + badCount > 0 ? (goodCount-badCount)/(goodCount+neutralCount+badCount) : 0}</p>
-      <p>positive</p>
-      <p>{goodCount + neutralCount + badCount > 0 ? (goodCount/(goodCount+neutralCount+badCount))*100 : 0} %</p>
-         
-
-  
+      <StatisticLine text="good" value={goodCount} />
+      <StatisticLine text="neutral" value={neutralCount} />
+      <StatisticLine text="bad" value={badCount} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + " %"} />
   </>
   )
 }
-
-
-
 function App() {
   const [goodCount, setGoodCount] = useState(0)
   const [neutralCount, setNeutralCount] = useState(0)
   const [badCount, setBadCount] = useState(0)
-
-
   return (
     <>
     <h1>Give feedback</h1>
       <button onClick={()=>setGoodCount(goodCount+1)}> good </button>
       <button onClick={()=>setNeutralCount(neutralCount+1)}> nuetral </button>
       <button onClick={()=>setBadCount(badCount+1)}> bad </button>
-
-     <Statistics badCount = {badCount} neutralCount = {neutralCount} goodCount = {goodCount} />
+     
+     {(goodCount + neutralCount + badCount) > 0 ? 
+     <Statistics badCount = {badCount} neutralCount = {neutralCount} goodCount = {goodCount} /> 
+     : 
+      <p>No feedback given</p>
+     }
     </>
   )
 }
